5f477eeeafbfe96ef2ab2bff08fef7f33d6d3df3
Anri Liy <amedamachip713@gmail.com>
2021-05-28 18:23:39 -0700

    Post method

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const http = require('http');
+const fs = require('fs');
 
-function rqListener(req, res) {
-  
-}
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>My First App</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(`Chunk: ${chunk}`);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(`parsedBody: ${parsedBody}`);
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First App</title></head>');
+  res.write('<body><h1>Hello world</h1></body>');
+  res.write('</html>');
 
-http.createServer();
+  res.end();
+});
+
+server.listen(5000);
